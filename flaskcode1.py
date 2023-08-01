00ae7473c141126d65d662ce35338c72eb1fc002
Your Name <you@example.com>
2023-08-01 08:06:53 +0530

    Rendering template is added

--- flaskcode1.py
+++ flaskcode1.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask,request,render_template,jsonify
 from flask import request
 
 app=Flask(__name__)
+
+@app.route('/',methods=['GET','POST'])
+def home_page():
+    return render_template('index.html')
 
 @app.route("/")
 def hello_world():
