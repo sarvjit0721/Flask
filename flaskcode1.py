0790d9911016a4fe674ce84c71b73e27e9e76e0e
Your Name <you@example.com>
2023-08-01 06:14:32 +0530

    Added new route

--- flaskcode1.py
+++ flaskcode1.py
@@ -12,6 +12,9 @@ def hello_test():
     data=request.args.get('x')
     return "This is input:{}".format(data)
 
+@app.route("/loggin")
+def hello_loggin():
+    return "<h2>I am logged in<h2>"
 
 if __name__=='__main__':
     app.run(host="0.0.0.0")
