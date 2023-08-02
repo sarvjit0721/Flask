22d2f692280cb3dc922134ea73de002a38a5820c
Your Name <you@example.com>
2023-08-02 08:03:01 +0530

    Connected apis to form

--- flaskcode1.py
+++ flaskcode1.py
@@ -7,6 +7,32 @@ app=Flask(__name__)
 def home_page():
     return render_template('index.html')
 
+@app.route('/math',methods=['POST'])
+def math_ops():
+    operation=request.form['operation']
+    num1=int(request.form['num1'])
+    num2=int(request.form['num2'])
+    if operation=='add':
+        result=num1+num2
+        addresult=f"Addition of {num1} and {num2} is: {result} "
+
+    elif operation=='substract':
+        result=abs(num1-num2)
+        addresult=f"Substarction of {num1} and {num2} is: {result} "
+
+    elif operation=='multiply':
+        result=num1*num2
+        addresult=f"Multiplication of {num1} and {num2} is: {result} "
+
+    elif operation=='divide':
+        result=num1/num2
+        addresult=f"Division of {num1} and {num2} is: {result} "
+
+    else:
+        addresult=f"{operation} is either not arithmetic or its not supported"
+    return render_template('results.html',result=addresult)
+   
+
 @app.route("/")
 def hello_world():
     return "<h1> Hello, Python </h1>"
